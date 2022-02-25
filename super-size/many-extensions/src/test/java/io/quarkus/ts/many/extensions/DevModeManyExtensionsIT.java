c437c62f860df38b7c32914bfd12794a4e1a0165
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-02-25 16:16:52 +0100

    Redpanda bug reproducer

--- super-size/many-extensions/src/test/java/io/quarkus/ts/many/extensions/DevModeManyExtensionsIT.java
+++ super-size/many-extensions/src/test/java/io/quarkus/ts/many/extensions/DevModeManyExtensionsIT.java
@@ -1,12 +1,17 @@
 package io.quarkus.ts.many.extensions;
 
 import io.quarkus.test.bootstrap.DevModeQuarkusService;
+import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.DevModeQuarkusApplication;
 
 @QuarkusScenario
 public class DevModeManyExtensionsIT extends ManyExtensionsIT {
+    private static final String RED_PANDA_VERSION = "v21.10.3";
+    private static final String RED_PANDA_IMAGE = "quay.io/quarkusqeteam/redpanda";
 
     @DevModeQuarkusApplication
-    static DevModeQuarkusService app = new DevModeQuarkusService();
+    static RestService app = new DevModeQuarkusService()
+            .withProperty("quarkus.kafka.devservices.enabled", Boolean.TRUE.toString())
+            .withProperty("quarkus.kafka.devservices.image-name", String.format("%s:%s", RED_PANDA_IMAGE, RED_PANDA_VERSION));
 }
